50128ca23c111602312bc96a7b6bf4b0a9b102e5
Jarrett Delaney <jarrettdelaney1@gmail.com>
2024-01-04 18:52:52 -0800

    Created DB logic in both the schema and seeds file, and added notes explaining the code.

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+-- Created a table to store dept. information. 
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY, -- Identifier for each department auto increments. 
+    name VARCHAR(30) NOT NULL -- Name of the department cannot be null. 
+);
+
+-- Created a table to store roles within the company. 
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY, -- Identifier for each department auto increments.
+    title VARCHAR(30) NOT NULL, -- Name of the role cannot be null.
+    salary DECIMAL NOT NULL, -- The salary amount cannot be null.
+    department_id INT, -- Links to the department the role belongs to.
+    FOREIGN KEY (department_id) REFERENCES department(id) -- Foreign key to the department table. 
+);
+
+-- Created a table to store employee info. 
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY, -- Identifier for each employee auto-increments.
+    first_name VARCHAR(30) NOT NULL, -- The first and last name for the employee cannot be null. 
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT, 
+    manager_id INT NULL, -- The employees manager cannot be null. 
+    FOREIGN KEY (role_id) REFERENCES role(id), -- Foreign key to role table.
+    FOREIGN KEY (manager_id) REFERENCES employee(id) -- Foreign key within employee table under the manager. 
+);
---
+++ db/seeds.sql
@@ -0,0 +1,19 @@
+--Inserted four department names into the department table. 
+INSERT INTO department (name) VALUES ('Engineering');
+INSERT INTO department (name) VALUES ('Human Resources');
+INSERT INTO department (name) VALUES ('Finance');
+INSERT INTO department (name) VALUES ('Marketing');
+
+-- Inserted the job title, salary, and department id. Each id number is based off the order of department names added into the department table. 
+INSERT INTO role (title, salary, department_id) VALUES ('Software Engineer', 70000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ('Senior Software Engineer', 90000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ('HR Manager', 75000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ('Accountant', 65000, 3);
+INSERT INTO role (title, salary, department_id) VALUES ('Marketing Director', 60000, 4);
+
+-- Adding the emplees names, role id, and manager id. 
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('John', 'Doe', 1, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Jim', 'Bob', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Ricky', 'Bobby', 3, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Bob', 'Builder', 4, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Emma', 'Stone', 5, 4);
